c5575453221670b044e4f5467ef4a531ec98ca18
zboro93 <34000498+zboro93@users.noreply.github.com>
2025-11-07 14:38:11 +0100

    Add S3 backend configuration for Terraform
    Configure S3 backend for Terraform state management.

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,13 @@ terraform {
       version = "~> 4.1"
     }
   }
+  backend "s3" {
+    bucket         = "zboro93-tfstate-bucket"
+    key            = "envs/dev/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "zboro93-terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
